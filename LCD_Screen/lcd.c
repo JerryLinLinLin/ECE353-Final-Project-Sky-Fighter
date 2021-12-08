3362071c163da75bfc7f0331004be41e0dbac692
Andy <Andy@10.141.176.7>
2021-12-08 09:03:51 -0600

    Added UART

--- LCD_Screen/lcd.c
+++ LCD_Screen/lcd.c
@@ -5,6 +5,7 @@
  *      Author: Andres Quintanal Escandon
  */
 
+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
 #include "lcd.h"
 
 /* ****************************************************************************
